e2b006cfce2ad8c1263f81f7b6dc3e28270bbf5a
Kegen Guyll <guyllkegen@gmail.com>
2021-09-19 15:58:43 -0500

    added redirect to comment page on mobile

--- components/card/cart-footer.tsx
+++ components/card/cart-footer.tsx
@@ -1,8 +1,10 @@
 import { Dispatch, FunctionComponent, SetStateAction } from 'react';
+import Link from 'next/link';
 
 interface Props {
   pointsCount: number;
   commentsCount: number;
+  postId: number;
   setShowComments: Dispatch<SetStateAction<boolean>>;
 }
 
@@ -10,13 +12,17 @@ const CardFooter: FunctionComponent<Props> = ({
   pointsCount,
   commentsCount,
   setShowComments,
+  postId,
 }) => {
   return (
     <div className='p-5 flex flex-row justify-end'>
       <p className='mr-5 cursor-pointer hover:underline'>{`${pointsCount} Points`}</p>
       <p
         onClick={() => setShowComments(true)}
-        className='cursor-pointer hover:underline'>{`${commentsCount} Comments`}</p>
+        className='cursor-pointer hover:underline hidden lg:block'>{`${commentsCount} Comments`}</p>
+      <Link passHref href={`/${postId}/comments`}>
+        <p className='cursor-pointer hover:underline block lg:hidden'>{`${commentsCount} Comments`}</p>
+      </Link>
     </div>
   );
 };
